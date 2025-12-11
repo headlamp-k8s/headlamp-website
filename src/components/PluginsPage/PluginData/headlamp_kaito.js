69f89457451968d635e3355ad3d323d02925f407
Evangelos Skopelitis <eskopelitis@microsoft.com>
2025-12-11 13:02:14 -0500

    Update KAITO plugin info on plugins page
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- src/components/PluginsPage/PluginData/headlamp_kaito.js
+++ src/components/PluginsPage/PluginData/headlamp_kaito.js
@@ -2,10 +2,10 @@
     const headlamp_kaito = {
     "version": "0.0.7",
     "name": "headlamp-kaito",
-    "displayName": "Headlamp Kaito",
+    "displayName": "Headlamp-KAITO",
     "createdAt": "2025-08-08T13:25:50Z",
     "logoURL": "https://avatars.githubusercontent.com/u/186863079?s=48&v=4",
-    "description": "This release 0.0.7 adds a SKU selector for GPU autoprovisioning mode with popular Azure and AWS instance types.",
+    "description": "Enhances the KAITO VSCode Extension with a modern, intuitive UI built directly into Headlamp.",
     "annotations": {
         "headlamp/plugin/archive-url": "https://github.com/kaito-project/headlamp-kaito/releases/download/0.0.7/headlamp-kaito-0.0.7.tar.gz",
         "headlamp/plugin/archive-checksum": "SHA256:a7228766e7b8d8413b44cdc727722a51a6825c61a1d0f052c06adab67a882ef6"
